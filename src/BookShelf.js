0e7b48fd92fcb68a4ff5e15b8c70bbbe62d72343
Nick Anthony <nicholas_anthony@brown.edu>
2018-10-24 08:38:58 -0400

    Refactored ShelfChanger into it's own component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import ShelfChanger from './ShelfChanger'
 
 class BookShelf extends Component {
   render() {
@@ -13,16 +13,7 @@ class BookShelf extends Component {
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.coverURL + '")' }}/>
-                    
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
+                    <ShelfChanger />
                   </div>
                   <div className="book-title">{book.title}</div>
                   <div className="book-authors">{book.author}</div>
