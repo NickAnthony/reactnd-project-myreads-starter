8e5b2e0f2b40c66f56ad0f13f3dff89ec681ce52
Nick Anthony <nicholas_anthony@brown.edu>
2018-10-28 14:31:54 -0400

    UpdateShelf now calls the update API

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -55,7 +55,7 @@ class SearchBooks extends Component {
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}/>
-                    <ShelfChanger changeShelf={this.props.changeShelf} bookTitle={book.title}/>
+                    <ShelfChanger changeShelf={this.props.changeShelf} book={book}/>
                   </div>
                   <div className="book-title">{book.title}</div>
                   { book.authors.map((author) =>
--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-
+import { update } from './BooksAPI'
 
 class ShelfChanger extends Component {
+  handleUpdateShelf = (new_shelf) => {
+    update(this.props.book, new_shelf).then(res => {
+        console.log(res)
+      }
+    )
+  }
   render() {
     return(
       <div className="book-shelf-changer">
-        <select onChange={(e) => this.props.changeShelf(this.props.bookTitle, e.target.value)}>
+        <select onChange={(e) => this.handleUpdateShelf(e.target.value)}>
           <option value="move" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
